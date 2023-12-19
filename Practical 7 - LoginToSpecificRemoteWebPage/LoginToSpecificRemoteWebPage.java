df96007b3e76855680be10528e476533c5ba5c21
Shridhar Pandey <63766338+IAmShridharr@users.noreply.github.com>
2023-12-19 13:16:49 +0530

    Update LoginToSpecificRemoteWebPage.java

--- Practical 7 - LoginToSpecificRemoteWebPage/LoginToSpecificRemoteWebPage.java
+++ Practical 7 - LoginToSpecificRemoteWebPage/LoginToSpecificRemoteWebPage.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginToSpecificRemoteWebPage {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://www.facebook.com/login/");
-        chromeDriver.findElement(By.id("email")).sendKeys(""); // Write your Facebook email or phone
-        chromeDriver.findElement(By.id("pass")).sendKeys(""); // Write your Facebook password
-        chromeDriver.findElement(By.id("loginbutton")).click();
+        chromeDriver.get("https://github.com/login");
+        chromeDriver.findElement(By.id("login_field")).sendKeys(""); // Write your Github Email or Username
+        chromeDriver.findElement(By.id("password")).sendKeys(""); // Write your Github Password
+        chromeDriver.findElement(By.name("commit")).click();
     }
 }
